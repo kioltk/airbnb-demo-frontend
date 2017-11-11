d6bfbe83e5d64a9f193bee2ba26765ba8fb8e126
David <kioltk@gmail.com>
2017-11-11 22:14:47 +0200

    Fixed content wrapper

--- src/Wrap.jsx
+++ src/Wrap.jsx
@@ -4,23 +4,16 @@ import Media from './Media';
 
 const Wrapper = styled.div`
   width: 100%;
+  box-sizing: border-box;
   margin-left: auto;
   margin-right: auto;
-  ${Media.lg`max-width: 1240px;`};
+  padding-right: 8px;
+  padding-left: 8px;
+  ${Media.xl`max-width: 1200px;`};
 `;
-const Container = styled.div`
-  padding-left: 0px;
-  padding-right: 0px;
 
-  ${Media.sm`padding-left:0; padding-right:0;`};
-  ${Media.md`padding-left:16px; padding-right:16px;`};
-  ${Media.lg`padding-left:80px; padding-right:80px;`};
-  ${Media.xl`padding-left:80px; padding-right:80px;`};
-`;
 export default props => (
   <Wrapper>
-    <Container>
-      <div className={props.className}>{props.children}</div>
-    </Container>
+    <div className={props.className}>{props.children}</div>
   </Wrapper>
 );
